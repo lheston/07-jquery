55a2aa78a59f6f334b0cfa78b3913bd29cf97118
lheston <loomisheston@hotmail.com>
2016-06-29 17:35:12 -0700

    test

--- js/warmup.js
+++ js/warmup.js
@@ -1,16 +1,39 @@
+//event handling practice
+/*var h1 = document.querySelector('h1');
+h1.addEventListener('click', function(event){
+    console.log("You clicked on " + event.target);
+});
+*/
+
 'use strict';
 
 //Add an event listener so that when the mouse enters ('mouseenter')
 //the image, it's `src` changes to 'img/surprised.png'
+
+var img = document.querySelector('img');
+
+img.addEventListener('mouseenter', function(){
+    img.setAttribute('src', 'img/surprised.png');
+});
 
 
 //Extra: add an event listener so that when the mouse leaves ('mouseleave')
 //the image, it's source changes back to `img/happy.png'
 
 
+img.addEventListener('mouseleave', function(){
+    img.setAttribute('src', 'img/happy.png');
+});
 //Add an event listener so that when the 'purple-button' is clicked,
 //the 'big' and 'purple' classes are added to the 'instructions'
 //paragraph in the header.
+
+var purp = document.querySelector('#purple-button');
+var inst = document.querySelector('.instructions')
+
+purp.addEventListener('click', function(){
+    inst.classList.add('big', 'purple');
+});
 
 
 //Add an event listener so that when the 'content-button' is clicked,
@@ -19,3 +42,11 @@
 //    TIP: You can get what value is typed into the <input> but accessing
 //         the elements '.value' property
 
+var contentBtn = document.querySelector('#content-button');
+contentBtn.addEventListener('click', function(){
+    var text = document.querySelector('input').value;
+    console.log(text);
+    var p = document.createElement('p');
+    p.textContent = text;
+    document.querySelector('#content').appendChild(p);
+});
