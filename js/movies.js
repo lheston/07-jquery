ec2e9fcb775b75ee754528b5c4bb40e9ea0a9d6b
lheston <loomisheston@hotmail.com>
2016-06-29 19:15:09 -0700

    update2

--- js/movies.js
+++ js/movies.js
@@ -1,3 +1,17 @@
 'use strict';
 
 /* code goes here */
+
+$('#myform').submit(function(event) {
+   
+   /* do whatever we want with the form's data */
+   var searchTerm = $('#movieTitle').val();
+
+   var url = 'http://omdbapi.com/?'+searchTerm;
+
+   console.log(url);
+   $.get(url);
+   //don't submit as usual!
+   event.preventDefault();    //current standard
+   return false;              //older browsers
+});
